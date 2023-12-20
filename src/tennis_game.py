eeca74282a5337e117be2bf8c9ca23c21c72f4dc
Gorka Moreno Asín <gorka@540deg.com>
2023-12-20 22:26:33 +0100

    Refactor and improve code

--- src/tennis_game.py
+++ src/tennis_game.py
@@ -1,46 +1,61 @@
 class TennisGame:
-    def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+    def __init__(self, player1_name: str, player2_name: str):
+        self.__player1_name: str = player1_name
+        self.__player2_name: str = player2_name
+        self.__player1_points: int = 0
+        self.__player2_points: int = 0
 
-    def won_point(self, playerName):
-        if playerName == "player1":
-            self.p1points += 1
+    def won_point(self, player_name: str):
+        if player_name == self.__player1_name:
+            self.__player1_points += 1
+        elif player_name == self.__player2_name:
+            self.__player2_points += 1
         else:
-            self.p2points += 1
+            raise ValueError("User not found")
 
-    def score(self):
-        result = ""
-        tempScore = 0
-        if self.p1points == self.p2points:
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.p1points >= 4 or self.p2points >= 4:
-            minusResult = self.p1points - self.p2points
-            if minusResult == 1:
-                result = "Advantage player1"
-            elif minusResult == -1:
-                result = "Advantage player2"
-            elif minusResult >= 2:
-                result = "Win for player1"
-            else:
-                result = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    tempScore = self.p1points
-                else:
-                    result += "-"
-                    tempScore = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[tempScore]
-        return result
+    def score(self) -> str:
+        if self.__is_tie():
+            tie_scores: list[str] = ["Love-All", "Fifteen-All", "Thirty-All"]
+            return (
+                "Deuce"
+                if self.__player1_points > 2
+                else tie_scores[self.__player1_points]
+            )
+
+        if self.__any_player_has_advantage():
+            return (
+                "Advantage player1"
+                if self.__player1_points_are_greater_than_player2_points()
+                else "Advantage player2"
+            )
+
+        if self.__any_player_wins():
+            return (
+                "Win for player1"
+                if self.__player1_points_are_greater_than_player2_points()
+                else "Win for player2"
+            )
+
+        scores: list[str] = ["Love", "Fifteen", "Thirty", "Forty"]
+        return f"{scores[self.__player1_points]}-{scores[self.__player2_points]}"
+
+    def __player1_points_are_greater_than_player2_points(self) -> bool:
+        return self.__player1_points > self.__player2_points
+
+    def __is_tie(self) -> bool:
+        return self.__player1_points == self.__player2_points
+
+    def __any_player_has_advantage(self) -> bool:
+        return (
+            self.__any_player_has_at_least_four_points()
+            and abs(self.__player1_points - self.__player2_points) == 1
+        )
+
+    def __any_player_wins(self) -> bool:
+        return (
+            self.__any_player_has_at_least_four_points()
+            and abs(self.__player1_points - self.__player2_points) > 1
+        )
+
+    def __any_player_has_at_least_four_points(self) -> bool:
+        return self.__player1_points >= 4 or self.__player2_points >= 4
